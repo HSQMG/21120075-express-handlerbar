66379b85cf2de6ae3bfe7c444e9cca46548ed39e
HSQMG <9981hoang@gmail.com>
2024-05-11 14:45:52 +0700

    add

---
+++ routes/task4Route.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const route = express.Router();
+const controller = require('../controllers/controller');
+route.get('/', controller.task4List);
+route.get('/:name', controller.task4Details);
+module.exports = route;
